48e6f737537ad6f6ef9f4e3282efdc75a0e9c22d
Jude Kajura <kbjude@gmail.com>
2020-11-11 21:55:30 +0300

    Adding buttons

--- src/scenes/titleScene.js
+++ src/scenes/titleScene.js
@@ -9,5 +9,23 @@ export default class TitleScene extends Phaser.Scene {
   }
 
   create() {
+    // Game
+    this.gameButton = this.add.sprite(100, 200, 'blueButton1').setInteractive();
+    this.centerButton(this.gameButton, 1);
+
+    this.gameText = this.add.text(0, 0, 'Play', { fontSize: '32px', fill: '#fff' });
+    this.centerButtonText(this.gameText, this.gameButton);
+
+    this.gameButton.on('pointerdown', (pointer) => {
+      this.scene.start('Game');
+    });
+
+    this.input.on('pointerover', (event, gameObjects) => {
+      gameObjects[0].setTexture('blueButton2');
+    });
+
+    this.input.on('pointerout', (event, gameObjects) => {
+      gameObjects[0].setTexture('blueButton1');
+    });
   }
 }
