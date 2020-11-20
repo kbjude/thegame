c31762074417f8fe2e127346d6eb7c2a36e31c7c
Jude Kajura <kbjude@gmail.com>
2020-11-21 00:40:36 +0300

    WIP motion

--- src/scenes/gameScene.js
+++ src/scenes/gameScene.js
@@ -1,5 +1,6 @@
 import Phaser from 'phaser';
 
+
 export default class GameScene extends Phaser.Scene {
   constructor() {
     super('Game');
@@ -8,9 +9,19 @@ export default class GameScene extends Phaser.Scene {
   preload() {
     // load images
     this.load.image('person', 'assets/person.png');
+
   }
 
   create() {
-    this.add.image(400, 540, 'person');
+    this.player = this.add.image(400, 540, 'person');
+    this.player.physics.add.sprite(100, 'person');
+    this.cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  update() {
+    const moveAmt = 200;
+    this.player.setDisplayOrigin(2000);
+
+    if (this.cursors.right.isDown) this.player.setVelocityX(moveAmt);
   }
 }
