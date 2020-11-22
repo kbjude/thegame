cd94c382caa1d89743e1044ecbca8970347ce401
Jude Kajura <kbjude@gmail.com>
2020-11-22 17:33:16 +0300

    changing the files

--- src/scenes/gameScene.js
+++ src/scenes/gameScene.js
@@ -1,34 +1,82 @@
-import Phaser from 'phaser';
-
-
-export default class GameScene extends Phaser.Scene {
+class SceneMain extends Phaser.Scene {
   constructor() {
-    super('Game');
+    super({ key: 'SceneMain' });
   }
 
   preload() {
-    // load images
-    // this.load.audio("sndExplode0", "content/sndExplode0.wav");
-    // this.load.audio("sndExplode1", "content/sndExplode1.wav");
-    // this.load.audio("sndLaser", "content/sndLaser.wav");
+  //   this.load.image(imageKey, path);
+  //   this.load.image('sprBg0', 'content/sprBg0.png');
+  //   this.load.image('sprBg0', 'content/sprBg0.png');
+  //   this.load.image('sprBg1', 'content/sprBg1.png');
+  //   this.load.spritesheet('sprExplosion', 'content/sprExplosion.png', {
+  //     frameWidth: 32,
+  //     frameHeight: 32,
+  //   });
+  //   this.load.spritesheet('sprEnemy0', 'content/sprEnemy0.png', {
+  //     frameWidth: 16,
+  //     frameHeight: 16,
+  //   });
+  //   this.load.image('sprEnemy1', 'content/sprEnemy1.png');
+  //   this.load.spritesheet('sprEnemy2', 'content/sprEnemy2.png', {
+  //     frameWidth: 16,
+  //     frameHeight: 16,
+  //   });
+  //   this.load.image('sprLaserEnemy0', 'content/sprLaserEnemy0.png');
+  //   this.load.image('sprLaserPlayer', 'content/sprLaserPlayer.png');
+  //   this.load.spritesheet('sprPlayer', 'content/sprPlayer.png', {
+  //     frameWidth: 16,
+  //     frameHeight: 16,
+  //   });
+    // this.load.audio('sndExplode0', 'content/sndExplode0.wav');
+    // this.load.audio('sndExplode1', 'content/sndExplode1.wav');
+    // this.load.audio('sndLaser', 'content/sndLaser.wav');
   }
 
   create() {
-    this.player = this.physics.add.sprite(400, 530, 'person');
-    this.cursors = this.input.keyboard.createCursorKeys();
-  }
+    // this.anims.create({
+    //   key: 'sprEnemy0',
+    //   frames: this.anims.generateFrameNumbers('sprEnemy0'),
+    //   frameRate: 20,
+    //   repeat: -1,
+    // });
 
-  update() {
-    const moveAmt = 200;
+    // this.anims.create({
+    //   key: 'sprEnemy2',
+    //   frames: this.anims.generateFrameNumbers('sprEnemy2'),
+    //   frameRate: 20,
+    //   repeat: -1,
+    // });
 
-    if (this.cursors.right.isDown) {
-      this.player.body.setVelocityX(moveAmt);
-    } else if (this.cursors.left.isDown) {
-      this.player.body.setVelocityX(-moveAmt);
-    } else if (this.cursors.down.isDown) {
-      this.player.body.setVelocityY(moveAmt);
-    } else if (this.cursors.up.isDown) {
-      this.player.body.setVelocityY(-moveAmt);
-    }
+    // this.anims.create({
+    //   key: 'sprExplosion',
+    //   frames: this.anims.generateFrameNumbers('sprExplosion'),
+    //   frameRate: 20,
+    //   repeat: 0,
+    // });
+
+    // this.anims.create({
+    //   key: 'sprPlayer',
+    //   frames: this.anims.generateFrameNumbers('sprPlayer'),
+    //   frameRate: 20,
+    //   repeat: -1,
+    // });
+
+    // this.sfx = {
+    //   explosions: [
+    //     this.sound.add('sndExplode0'),
+    //     this.sound.add('sndExplode1'),
+    //   ],
+    //   laser: this.sound.add('sndLaser'),
+    // };
+
+    // this.load.image('sprBtnPlay', 'content/sprBtnPlay.png');
+    // this.load.image('sprBtnPlayHover', 'content/sprBtnPlayHover.png');
+    // this.load.image('sprBtnPlayDown', 'content/sprBtnPlayDown.png');
+    // this.load.image('sprBtnRestart', 'content/sprBtnRestart.png');
+    // this.load.image('sprBtnRestartHover', 'content/sprBtnRestartHover.png');
+    // this.load.image('sprBtnRestartDown', 'content/sprBtnRestartDown.png');
+
+    // this.load.audio('sndBtnOver', 'content/sndBtnOver.wav');
+    // this.load.audio('sndBtnDown', 'content/sndBtnDown.wav');
   }
 }
