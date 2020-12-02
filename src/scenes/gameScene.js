3a447a9279086e388e5397156f018ac8dcf8a44e
Jude Kajura <kbjude@gmail.com>
2020-12-02 04:13:27 +0300

    Working game with bullets

--- src/scenes/gameScene.js
+++ src/scenes/gameScene.js
@@ -214,6 +214,13 @@ export default class SceneMain extends Phaser.Scene {
       this.player.moveRight();
     }
 
+    if (this.keySpace.isDown) {
+      this.player.setData('isShooting', true);
+    } else {
+      this.player.setData('timerShootTick', this.player.getData('timerShootDelay') - 1);
+      this.player.setData('isShooting', false);
+    }
+
     for (let i = 0; i < this.enemies.getChildren().length; i++) {
       const enemy = this.enemies.getChildren()[i];
 
